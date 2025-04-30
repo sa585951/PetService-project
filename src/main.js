0f3b0c7cae466e6756871206a950d965bdb16d97
sa585951 <sa58595189@gmail.com>
2025-05-01 01:16:22 +0800

    安裝pinia狀態管理套件

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+
+const app = createApp(App);
+app.use(router);
+app.use(createPinia());
+app.mount('#app');
